b3d10157c6a190be16f3e08f1db9d107a8525ce8
Tharindu Dias <tkdias0@gmail.com>
2020-01-06 17:31:49 +0530

    import bootstrap and mdb

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<script>
+	setTimeout(function () {window.location.href= 'section.php';},9000);
+	</script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Smart EE Checkup | Platform</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/mdb/css/bootstrap.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="bootstrap/mdb/css/mdb.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="bootstrap/mdb/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Start your code here --------------------------------------------------------- -->
+    <div style="height: 100vh">
+        <div class="flex-center">
+            <font  size="12" color="#FFFFFF" class="animated fadeInDownBig" align="center" >Automated Centralized <br>Smart EE Checkup
+Portal Network System <br> to Check the Vision and Hearing of The Patients</font>
+        </div>
+    </div>
+    <!-- /End your code here ---------------------------------------------------------- -->
+
+    <!-- SCRIPTS ---------------------------------------------------------------------- -->
+
+    <!-- JQuery -->
+    <script type="bootstrap/text/javascript" src="mdb/js/jquery-2.2.3.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="bootstrap/text/javascript" src="mdb/js/tether.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="bootstrap/text/javascript" src="mdb/js/bootstrap.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="bootstrap/text/javascript" src="mdb/js/mdb.min.js"></script>
+	<!-- JQuery -->
+    <script type="bootstrap/text/javascript" src="mdb/js/jquery-3.1.0.min.js"></script>
+
+
+</body>
+
+</html>
